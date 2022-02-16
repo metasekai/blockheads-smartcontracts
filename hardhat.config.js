865c90b8583030dfb5b4f4968c6e218756acaaee
Alleo Indong <alleo.indong@gmail.com>
2022-02-16 14:43:14 +0800

    hardhat config and added dev packages

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,14 +1,8 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+require('dotenv').config();
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
@@ -18,4 +12,15 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    testnet: {
+      url: process.env.MUMBAI_NODE,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: process.env.API_KEY
+  }
 };
